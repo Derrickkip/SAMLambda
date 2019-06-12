07a13295639a663ca62867ad313743f1ca066a3b
Derrick Korir <derrickkip@Derricks-MacBook-Pro.local>
2019-06-12 17:14:50 +0300

    Initial Commit

---
+++ get-ec2-regions/main.py
@@ -0,0 +1,35 @@
+import boto3
+import json
+
+# import requests
+ec2Client = boto3.client('ec2')
+
+
+def lambda_handler(event, context):
+    """Sample pure Lambda function
+
+    Parameters
+    ----------
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
+
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    ------
+    API Gateway Lambda Proxy Output Format: dict
+
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    """
+    # regionsRes = ec2Client.describe_regions()
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": 'My name is Derrick'
+        }),
+    }
